fde8139231585de5cf57d50b299104b998990910
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-08-07 15:22:22 +0300

    docker-compose.yml eklendi

--- src/main/java/dao/App.java
+++ src/main/java/dao/App.java
@@ -5,8 +5,8 @@ import model.Person;
 public class App {
     public static void main (String [] args){
        PersonDAOImpl personDAO = new PersonDAOImpl();
-        Person person=new Person(7,"ali","genc");
-        personDAO.updatePerson(person);
-       System.out.println(person);
+        Person p=new Person(7,"ali","genc");
+        personDAO.updatePerson(p);
+       System.out.println(p);
     }
 }
