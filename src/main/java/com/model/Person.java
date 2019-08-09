e4f58bbfd55a80748c24144969408a55b0bc9847
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-08-09 13:47:53 +0300

    lombok eklendi

--- src/main/java/com/model/Person.java
+++ src/main/java/com/model/Person.java
@@ -2,16 +2,15 @@ package com.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
     private int id;
     private String isim;
     private String soyisim;
-
-    public Person() {
-    }
 
 }
